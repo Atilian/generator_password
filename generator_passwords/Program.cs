08884b5176e4bc8b2522198e6d609ea60494bd44
Atilian <gascon.mickael@outlook.fr>
2022-12-30 16:54:14 +0100

    Ajout au dépot le program generator_passwords

---
+++ generator_passwords/Program.cs
@@ -0,0 +1,86 @@
+﻿
+
+namespace generator_password
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            app();
+        }
+
+        static void app()
+        {
+
+            Console.WriteLine("***********************\n" +
+                              "* Generator passwords *\n" +
+                              "***********************\n");
+
+
+            Console.Write("Password length : ");
+
+            int passSize;
+            int.TryParse(Console.ReadLine(), out passSize);
+
+            List<string> menu = new List<string>() 
+            { 
+                "Only lowercase characters", 
+                "Lowercase and uppercase characters", 
+                "Characters and numbers", 
+                "Characters, numbers and special characters"
+            };
+
+            Console.WriteLine("\nMenu\n");
+
+            for(int index = 0; index < menu.Count; index++)
+            {
+                Console.WriteLine(" {0} - {1}\n", index, menu[index]);
+            }
+
+
+            while(true)
+            {
+                Console.Write("\nChoice : ");
+
+                int choice;
+
+                if(int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine(generator(choice, passSize));
+                }
+                else
+                {
+                    Console.WriteLine("Write a number valid");
+                }
+            }
+
+            static string generator(int choice, int length)
+            {
+                List<string> alphabet = new List<string>()
+                {
+                    "a","b","c","d","e",
+                    "f","g","h","i","a",
+                    "a","a","a","a","a",
+                    "a","a","a","a","a",
+                    "a","a","a","a","a",
+                    "a",
+                };
+
+                switch (choice)
+                {
+                    case 1:
+
+                }
+
+                return "s";
+            }
+
+        }
+
+        
+
+    }
+
+}
